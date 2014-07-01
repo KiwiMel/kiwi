517d1c1dc80e2314da9dfe43875d7ae9e2d653d5
KiwiMel <mel.abercrombie@gmail.com>
2014-07-01 14:13:33 +0100

    Testing committing to git

---
+++ src/test/MelTest2.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by Mel on 01/07/2014.
+ */
+public class MelTest2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+
+}
